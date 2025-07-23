1287de5ff40e4c50d233d22b1c94b21d28a0dec5
Cristian Cordero <cristiancordero2810@gmail.com>
2025-07-23 03:59:43 -0500

    Ejercicio para entender el concepto de ficheros y las formas de implementar una especie de CRUD con su contenido

--- Manejo_de_ficheros.py
+++ Manejo_de_ficheros.py
@@ -1,3 +1,21 @@
 """
-Ejercicio 1: Manejo de ficheros
+Ejercicio: Manejo de ficheros
 """
+
+import os
+
+# Definir el nombre del fichero
+file_name = "cristianplus.txt"
+
+# Escribir en el fichero
+with open(file_name, "w") as file: 
+    file.write("Cristian Cordero\n") #Nombre
+    file.write("25\n") #Edad
+    file.write("JavaScript") #Lenguaje de programación
+
+# Leer el fichero y mostrar su contenido
+with open(file_name, "r") as file:
+    print(file.read())
+
+# Eliminar el fichero
+os.remove(file_name)
